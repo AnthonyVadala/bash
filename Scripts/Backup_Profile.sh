f8cd34653857fcabe8a16c8a6314c288ae91fdb9
Anthony Vadala <git@anthonyvadala.me>
2021-11-03 08:40:35 -0400

    Updated scripts

--- Scripts/Backup_Profile.sh
+++ Scripts/Backup_Profile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#Backup Profile - 12/31/2020
+#Backup Profile - 11/03/2021
 #Copies .bash_profile, bash scripts, PICO-8 carts, Brew Caskfile, and Brewfile to GitHub directory
 #Copies dotfiles to iCloud
 
 #.bash_profile copy
-echo "Copying .bash_profile..."
-cp ~/.bash_profile ~/Github/Terminal_Settings/Terminal/
-echo "Complete"
+#echo "Copying .bash_profile..."
+#cp ~/.bash_profile ~/Github/Terminal_Settings/Terminal/
+#echo "Complete"
 
 #.zshrc copy
 echo "Copying .zshrc..."
-cp ~/.zshrc ~/Github/Terminal_Settings/Terminal/
+cp ~/.zshrc ~/GitHub/Terminal_Settings/Terminal/
 echo "Complete"
 
 #Bash scripts copy
@@ -45,24 +45,9 @@ echo "Complete"
 
 #Dotfiles
 echo "Copying dotfiles..."
-cp -R ~/.balena ~/Documents/Dotfiles
-cp -R ~/.bash_history ~/Documents/Dotfiles
-cp -R ~/.bash_profile ~/Documents/Dotfiles
 cp -R ~/.config ~/Documents/Dotfiles
-cp -R ~/.cordless ~/Documents/Dotfiles
 cp -R ~/.gem ~/Documents/Dotfiles
 cp -R ~/.gitconfig ~/Documents/Dotfiles
-cp -R ~/.mono ~/Documents/Dotfiles
-cp -R ~/.mputils ~/Documents/Dotfiles
-cp -R ~/.msf4 ~/Documents/Dotfiles
-cp -R ~/.mume ~/Documents/Dotfiles
-cp -R ~/.rainbow_config.json ~/Documents/Dotfiles
-cp -R ~/.rainbow_oauth ~/Documents/Dotfiles
-cp -R ~/.rbenv ~/Documents/Dotfiles
-cp -R ~/.runelite ~/Documents/Dotfiles
 cp -R ~/.ssh ~/Documents/Dotfiles
-cp -R ~/.viminfo ~/Documents/Dotfiles
-cp -R ~/.vscode ~/Documents/Dotfiles
 cp -R ~/.zshrc ~/Documents/Dotfiles
-cp -R ~/.zshrc.pre-oh-my-zsh ~/Documents/Dotfiles
 echo "Complete"
